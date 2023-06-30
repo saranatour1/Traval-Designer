c858f581b96f09e322f816ba60c54087323ac93d
Sara <saranatourcontact@gmail.com>
2023-06-30 18:54:15 +0300

    Folder Structure

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className=' text-justify'>Helloooo</h1>
+        
+        </>
+  )
+}
+
+export default App
