7987d37f324719914e7500dcfd2e81733408a349
Sara <saranatourcontact@gmail.com>
2023-07-02 22:44:31 +0300

    to list, add , edit ,delete , view

--- server/controllers/todo.controllers.js
+++ server/controllers/todo.controllers.js
@@ -1,23 +1,108 @@
 const Trip = require("../models/trip.model");
 
-module.exports ={
-  // add a list of items to the db, it 
+module.exports = {
+  // create a set of list items
   create: async (req, res) => {
     const postId = req.params.postId;
-    const userId = req.params.userId;
     try {
       const todoListItems = req.body;
-      const doc = await Trip.findOneAndUpdate(
-        { _id: postId },
-        { $push: { toDoList: { ...todoListItems , updatedAt: new Date() } } },
-        { new: true }
-      );
-      console.log(doc);
 
+      const trip = await Trip.findOne({ _id: postId });
+      if (!trip) {
+        return res.status(404).json({ message: "Trip not found" });
+      }
+
+      //the added items must be an array of objects
+      const addedItems = todoListItems.map((item) => ({
+        ...item,
+        updatedAt: new Date(),
+      }));
+
+      trip.toDoList.push(...addedItems);
+      const updatedTrip = await trip.save();
+
+      console.log(updatedTrip.toDoList);
+
+      return res
+        .status(200)
+        .json({ message: "List added!", items: addedItems });
     } catch (error) {
       console.log(error);
+      return res.status(500).json({ message: "Internal server error" });
     }
-    return res.status(200).json({ message: "List added!"});
-  }
-  
-}
+  },
+  // get all the list items 
+  getAll: async (req, res) => {
+    const postId = req.params.postId;
+    try {
+      const trip = await Trip.findOne({ _id: postId });
+      if (!trip) {
+        return res.status(404).json({ message: "Trip not found!" });
+      }
+      const toDoList = trip.toDoList;
+      console.log(toDoList);
+      return res.status(200).json(toDoList);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  },
+  // editing all the items, small defect: when editing like this we are editing the whole entire thing
+  // so it will add on top of that, or delete all the previous items
+  editToDo: async (req, res) => {
+    const postId = req.params.postId;
+    try {
+      // map all over the items and add the updated at manually
+      const updatedToDoList = req.body.map((item) => ({
+        ...item,
+        updatedAt: new Date(),
+      }));
+
+      const trip = await Trip.findOneAndUpdate(
+        { _id: postId },
+        {
+          toDoList: updatedToDoList,
+          updatedAt: new Date(),
+        },
+        { new: true }
+      );
+
+      if (!trip) {
+        return res.status(404).json({ message: "Trip not found" });
+      }
+
+      return res.status(200).json({ message: "To-do list updated", trip });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  },
+
+  deleteItem: async (req, res) => {
+    const postId = req.params.postId;
+    const itemId = req.params.itemId;
+    console.log("I am deleting something ");
+    try {
+      const trip = await Trip.findOne({ _id: postId });
+
+      if (!trip) {
+        return res.status(404).json({ message: "Trip not found" });
+      }
+
+      trip.toDoList = trip.toDoList.filter(
+        (item) => item._id.toString() !== itemId
+      );
+
+      const updatedTrip = await trip.save();
+
+      return res
+        .status(200)
+        .json({ message: "To-do list item deleted", trip: updatedTrip });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  },
+
+  // delete all items? maybe not 
+};
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(cors({credentials: true, origin: 'http://localhost:5173'}));
 app.use(cookieParser());
 const allUsersRoutes = require("./routes/users.routes");
 const allTripRoutes = require('./routes/trip.routes');
+const allToDoListRoutes = require('./routes/todo.routes');
 // Change the app.use(cors()) to the one below
 
 
@@ -22,6 +23,7 @@ app.use(express.json(), express.urlencoded({ extended: true }));
 
 allUsersRoutes(app);
 allTripRoutes(app); 
+allToDoListRoutes(app);
 
 app.listen(8000, () => console.log("The server is all fired up on port 8000"));
 
